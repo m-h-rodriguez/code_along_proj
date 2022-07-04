fb15b313658f794c32c28e445af7d17d71433c89
m-h-rodriguez <michellehope100@gmail.com>
2022-07-04 10:49:42 -0400

    adding def test

--- test_def.py
+++ test_def.py
@@ -4,6 +4,7 @@ import pandas as pd
 import os
 import numpy as np
 import seaborn as sns
+import matplotlib.pyplot as plt
 
 # filtered values used to extract the max and min salary are setting a value on a copy of a slice from the dataframe.
 # To ignore this warning as a means of only pulling the values, the chained mode assignment sets this to none for it to ignore the warning
@@ -20,14 +21,25 @@ salary_data_df.dropna(subset=['Employee_Name'], inplace=True)
 salary_data_df.drop(
     salary_data_df[salary_data_df['CalYear'] != 2022].index, inplace=True)
 
-# print(salary_data_df.head())
+# print(salary_data_df.tail())
 
 
-def calc_salary(salary_data_df):
+def avg_salary(df):
     avg_salary = salary_data_df.groupby('Department', as_index=False)[
         'Annual_Rate'].mean()
-    dept = salary_data_df['Department'].unique()
-    return sns.histplot(data=avg_salary, x=dept, y=avg_salary)
+    dept_count = salary_data_df[['Department']
+                                ].value_counts().reset_index(name='counts')
+    #dept = salary_data_df['Department'].unique()
+    return avg_salary, dept_count
 
 
-print(calc_salary(salary_data_df))
+print(avg_salary(salary_data_df))
+
+
+# plt.plot(dept, salary, 'o')
+# plt.figure(figsize=(20, 5))
+# plt.scatter(dept, salary)
+# plt.xticks(dept[::1], rotation="vertical")
+# plt.xlabel('Department')
+# plt.ylabel('Salary Budget')
+# plt.show()
